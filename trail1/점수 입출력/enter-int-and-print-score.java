793665861a7aec878b24552e7be55a949c9bac43
Seoyun Ban <claire112207@gmail.com>
2026-08-22 15:25:34 +0900

    [Passed] 점수 입출력 (128ms, 10MB)

---
+++ trail1/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int point = sc.nextInt();
+        System.out.print("Your score is " + point + " point.");
+    }
+}
