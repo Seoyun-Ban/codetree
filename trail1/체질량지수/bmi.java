4ee5e792b1460384ea220346ecf9b4d8028b0318
Seoyun Ban <claire112207@gmail.com>
2026-09-08 02:09:33 +0900

    [Passed] 체질량지수 (126ms, 10MB)

---
+++ trail1/체질량지수/bmi.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt(), w = sc.nextInt();
+        double b = (10000 * w) / (h * h);
+        
+        System.out.println((int) b);
+
+        if (b >= 25){
+            System.out.print("Obesity");
+        }
+    }
+}
