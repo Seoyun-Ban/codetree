445c54b89020ed3769a41b7fad3dffbbe3646624
Seoyun Ban <claire112207@gmail.com>
2026-09-13 02:39:31 +0900

    [Passed] 세 수의 중간값 (135ms, 10MB)

---
+++ trail1/세 수의 중간값/median-of-three-numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt(), B = sc.nextInt(), C = sc.nextInt();
+
+        if (B > A && B < C){
+            System.out.print("1");
+        }
+        else{
+            System.out.print("0");
+        }
+    }
+}
