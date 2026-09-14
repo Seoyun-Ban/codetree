9b13b86346e646cee2fcb372d2c3bd5f1697be88
Seoyun Ban <claire112207@gmail.com>
2026-09-15 00:38:08 +0900

    [Passed] 장학금 (128ms, 10MB)

---
+++ trail1/장학금/scholarship.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int mid = sc.nextInt(), fin = sc.nextInt();
+
+        if (mid >= 90){
+            if (fin >= 95){
+                System.out.print("100000");
+            }
+            else if (fin >= 90){
+                System.out.print("50000");
+            }
+            else{
+                System.out.print("0");
+            }
+        }
+        else{
+            System.out.print("0");
+        }
+    }
+}
